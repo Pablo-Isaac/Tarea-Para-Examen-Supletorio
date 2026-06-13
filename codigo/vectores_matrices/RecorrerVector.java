31da8160cc4746d295102b6c979f10107127771f
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:07:01 -0500

    Update RecorrerVector.java

--- codigo/vectores_matrices/RecorrerVector.java
+++ codigo/vectores_matrices/RecorrerVector.java
@@ -1,3 +1,15 @@
 public class RecorrerVector {
 
+    public static void main(String[] args) {
+
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < numeros.length; i++) {
+
+            System.out.println(numeros[i]);
+
+        }
+
+    }
+
 }
