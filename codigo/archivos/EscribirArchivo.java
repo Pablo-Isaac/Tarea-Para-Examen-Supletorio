3dffade9d26160ada32091fa67bf7005341d0e43
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:08:25 -0500

    Update EscribirArchivo.java

--- codigo/archivos/EscribirArchivo.java
+++ codigo/archivos/EscribirArchivo.java
@@ -1,3 +1,26 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class EscribirArchivo {
+
+    public static void main(String[] args) {
+
+        try {
+
+            FileWriter escritor = new FileWriter("datos.txt");
+
+            escritor.write("Hola mundo desde Java");
+
+            escritor.close();
+
+            System.out.println("Archivo creado correctamente.");
+
+        } catch (IOException e) {
+
+            System.out.println("Error al escribir el archivo.");
+
+        }
+
+    }
 
 }
