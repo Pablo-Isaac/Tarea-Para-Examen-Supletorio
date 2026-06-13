815f2eee007709a02d3141a8ed1025b821383ffe
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:06:24 -0500

    Update MetodoRetorno.java

--- codigo/metodos/MetodoRetorno.java
+++ codigo/metodos/MetodoRetorno.java
@@ -1,3 +1,17 @@
 public class MetodoRetorno {
 
+    public static int multiplicar(int a, int b) {
+
+        return a * b;
+
+    }
+
+    public static void main(String[] args) {
+
+        int resultado = multiplicar(4, 6);
+
+        System.out.println("Resultado: " + resultado);
+
+    }
+
 }
