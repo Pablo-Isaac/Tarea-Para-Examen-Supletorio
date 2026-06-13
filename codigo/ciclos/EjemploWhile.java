665ef5d5e065ec962aabd8c09ffd4714afee452b
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:04:52 -0500

    Update EjemploWhile.java

--- codigo/ciclos/EjemploWhile.java
+++ codigo/ciclos/EjemploWhile.java
@@ -1,3 +1,16 @@
 public class EjemploWhile {
 
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        System.out.println("Numeros del 1 al 10 usando while:");
+
+        while (contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+
+    }
+
 }
