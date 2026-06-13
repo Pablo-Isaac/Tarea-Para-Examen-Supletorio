f615132636e0ca2d8b3d75644272ff6fb915150a
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:07:28 -0500

    Update ListaBasica.java

--- codigo/listas/ListaBasica.java
+++ codigo/listas/ListaBasica.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class ListaBasica {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> nombres = new ArrayList<>();
+
+        nombres.add("Juan");
+        nombres.add("Maria");
+        nombres.add("Carlos");
+
+        System.out.println(nombres);
+
+    }
 
 }
