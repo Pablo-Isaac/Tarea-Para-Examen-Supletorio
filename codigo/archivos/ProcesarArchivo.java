e0aa930e4a248dfc7c3f97c1417ad4f51b04590d
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:08:36 -0500

    Update ProcesarArchivo.java

--- codigo/archivos/ProcesarArchivo.java
+++ codigo/archivos/ProcesarArchivo.java
@@ -1,3 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ProcesarArchivo {
+
+    public static void main(String[] args) {
+
+        int contador = 0;
+
+        try {
+
+            BufferedReader lector = new BufferedReader(
+                    new FileReader("datos.txt")
+            );
+
+            while (lector.readLine() != null) {
+
+                contador++;
+
+            }
+
+            lector.close();
+
+            System.out.println("Cantidad de lineas: " + contador);
+
+        } catch (IOException e) {
+
+            System.out.println("Error al procesar el archivo.");
+
+        }
+
+    }
 
 }
