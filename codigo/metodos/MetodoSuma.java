3f8e1fef3eca60ba31921b7b71aecd390697564f
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:06:15 -0500

    Update MetodoSuma.java

--- codigo/metodos/MetodoSuma.java
+++ codigo/metodos/MetodoSuma.java
@@ -1,3 +1,17 @@
 public class MetodoSuma {
 
+    public static void sumar(int a, int b) {
+
+        int resultado = a + b;
+
+        System.out.println("La suma es: " + resultado);
+
+    }
+
+    public static void main(String[] args) {
+
+        sumar(10, 5);
+
+    }
+
 }
