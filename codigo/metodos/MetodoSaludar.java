092e7d17b33f84a9b9e51c5f7b69dfc809882c5e
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:06:04 -0500

    Update MetodoSaludar.java

--- codigo/metodos/MetodoSaludar.java
+++ codigo/metodos/MetodoSaludar.java
@@ -1,3 +1,13 @@
 public class MetodoSaludar {
 
+    public static void saludar() {
+        System.out.println("Hola, bienvenido al tutorial de Java.");
+    }
+
+    public static void main(String[] args) {
+
+        saludar();
+
+    }
+
 }
