b333e4611b8bd9005e115312f1c7340c95d429f0
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:04:36 -0500

    Update EjemploFor.java

--- codigo/ciclos/EjemploFor.java
+++ codigo/ciclos/EjemploFor.java
@@ -1,3 +1,13 @@
 public class EjemploFor {
 
+    public static void main(String[] args) {
+
+        System.out.println("Numeros del 1 al 10 usando for:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+
 }
