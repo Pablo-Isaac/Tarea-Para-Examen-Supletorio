487a935d50781d9368e9a40ffa90403209916d51
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:07:51 -0500

    Update RecorrerLista.java

--- codigo/listas/RecorrerLista.java
+++ codigo/listas/RecorrerLista.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+
 public class RecorrerLista {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> ciudades = new ArrayList<>();
+
+        ciudades.add("Quito");
+        ciudades.add("Guayaquil");
+        ciudades.add("Cuenca");
+
+        for (String ciudad : ciudades) {
+
+            System.out.println(ciudad);
+
+        }
+
+    }
 
 }
