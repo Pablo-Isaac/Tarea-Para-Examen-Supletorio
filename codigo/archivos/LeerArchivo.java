f0b01150acf6525d5b0cc2a3146aa4624b97ca40
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:08:13 -0500

    Update LeerArchivo.java

--- codigo/archivos/LeerArchivo.java
+++ codigo/archivos/LeerArchivo.java
@@ -1,3 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LeerArchivo {
+
+    public static void main(String[] args) {
+
+        try {
+
+            BufferedReader lector = new BufferedReader(
+                    new FileReader("datos.txt")
+            );
+
+            String linea;
+
+            while ((linea = lector.readLine()) != null) {
+
+                System.out.println(linea);
+
+            }
+
+            lector.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Error al leer el archivo.");
+
+        }
+
+    }
 
 }
