8a7be712dfe014f5542884af8c8cc9d4d06a63fe
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:07:11 -0500

    Update MatrizBasica.java

--- codigo/vectores_matrices/MatrizBasica.java
+++ codigo/vectores_matrices/MatrizBasica.java
@@ -1,3 +1,24 @@
 public class MatrizBasica {
 
+    public static void main(String[] args) {
+
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        for (int fila = 0; fila < matriz.length; fila++) {
+
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+
+                System.out.print(matriz[fila][columna] + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
 }
