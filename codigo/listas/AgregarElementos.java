ab9832235f8ef5c998d703cc8286aed0c65b2ef1
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:07:40 -0500

    Update AgregarElementos.java

--- codigo/listas/AgregarElementos.java
+++ codigo/listas/AgregarElementos.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class AgregarElementos {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> frutas = new ArrayList<>();
+
+        frutas.add("Manzana");
+        frutas.add("Banano");
+        frutas.add("Pera");
+
+        System.out.println(frutas);
+
+    }
 
 }
