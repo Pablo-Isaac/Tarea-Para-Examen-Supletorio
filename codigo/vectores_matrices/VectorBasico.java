3bc352d47223aff2dbc50ccd4335ee94207160ee
Pablo-Isaac <pablitosanchez5555@gmail.com>
2026-06-13 12:06:50 -0500

    Update VectorBasico.java

--- codigo/vectores_matrices/VectorBasico.java
+++ codigo/vectores_matrices/VectorBasico.java
@@ -1,3 +1,12 @@
 public class VectorBasico {
 
+    public static void main(String[] args) {
+
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        System.out.println("Primer elemento: " + numeros[0]);
+        System.out.println("Segundo elemento: " + numeros[1]);
+
+    }
+
 }
